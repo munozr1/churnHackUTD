cf1d4107ef1862bac4a466a716a07500e41a8794
Rodrigo Munoz <rmunoz@rigombp2019.local>
2023-11-05 04:28:33 -0600

    test:

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,11 +1,38 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 import Feed from '../../components/Feed';
 
 
 
 export default function Home() {
+  const [linkToken, setLinkToken] = React.useState(null);
+  // const address = Platform.OS === 'ios' ? 'localhost' : '10.0.2.2';
+  const address = 'localhost'
+
+  const createLinkToken = React.useCallback(async () => {
+    await fetch(`http://${address}:8080/api/create_link_token`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ address: address })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setLinkToken(data.link_token);
+      })
+      .catch((err) => {
+        console.log("createLinkToken => ",err);
+      });
+  }, [setLinkToken])
+
+  React.useEffect(() => {
+    if (linkToken == null) {
+      createLinkToken();
+    }
+  }, [linkToken]);
+
   return (
     <View style={[{width: '100%', height: '100%'}, styles.container]}>
       <LinearGradient
@@ -24,16 +51,15 @@ export default function Home() {
           <Text style={{ marginTop: 10 }}>
             September 21, 2020
           </Text>
+          
           <View style={[{marginTop: 65, width: '80%', height: 65, borderRadius: 15 , backgroundColor: 'white', alignItems: 'center', justifyContent: 'center'}, styles.shadow]}>
-            <Text style={{fontSize: 20, fontWeight: 600}}>
-              Make Payment
-            </Text>
+            <TouchableOpacity onPress={createLinkToken}>
+              <Text style={{ fontSize: 20, fontWeight: 600 }} >
+                Make Payment
+              </Text>
+            </TouchableOpacity>
           </View>
         </SafeAreaView>
-
-
-         
-
 
       </LinearGradient>
       <View style={{  width: '100%', marginTop: 445, height: '55%', marginLeft: 15}}>
